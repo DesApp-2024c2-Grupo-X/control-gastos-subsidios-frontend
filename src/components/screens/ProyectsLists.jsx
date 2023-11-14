20d0e6c06796fcb8fb720f6d22cce413135a5d4e
JulianDiani <72026658+JulianDiani@users.noreply.github.com>
2023-11-14 20:04:09 -0300

    Fix en listado de proyectos de admin, ahora se muestran los datos correctos

--- src/components/screens/ProyectsLists.jsx
+++ src/components/screens/ProyectsLists.jsx
@@ -12,6 +12,10 @@ import React, { useEffect, useState } from 'react';
 import { getProyectsForAdmin } from '../../services/proyectos';
 import { Link } from 'react-router-dom';
 import { formatDate } from '../../utils/validaciones';
+import {getTotalSubsidio} from '../../services/subsidiosasignados';
+import { calculateTotalExpenses } from '../../utils/presupuestos';
+import {getComprasByProyecto} from '../../services/compras'
+import {getConvocatoriaById} from '../../services/convocatorias'
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
@@ -49,13 +53,39 @@ export const ProyectsList = () => {
   };
   useEffect(() => {
     async function getProjects() {
-      const proyectos = await getProyectsForAdmin();
-      console.log(proyectos)
-      setProyects(proyectos);
-    }
-    getProjects();
-  }, []) //only de first render
+      try {
+        const proyectos = await getProyectsForAdmin();
   
+        // Recorrer la lista de proyectos y obtener totalSubsidio para cada uno
+        const proyectosConDatosAdicionales = await Promise.all(
+          proyectos.map(async (proyecto) => {
+            const totalSubsidio = await getTotalSubsidio(proyecto.id);
+            proyecto.SubsidiosAsignados = totalSubsidio;
+  
+            const compras = await getComprasByProyecto(proyecto.id);
+            const gastos = calculateTotalExpenses(compras);
+            const convocatoria=await getConvocatoriaById(proyecto.idConvocatoria)
+            proyecto.convocatoria=convocatoria
+  
+            // Agregar el campo gastos al proyecto
+            proyecto.gastos = gastos;
+  
+            return proyecto;
+          })
+        );
+  
+        console.log("PROYECTOS", proyectosConDatosAdicionales);
+        setProyects(proyectosConDatosAdicionales);
+      } catch (error) {
+        // Manejar errores si es necesario
+        console.error("Error al obtener proyectos:", error);
+      }
+    }
+  
+    getProjects();
+  }, []);
+  
+ 
   return (
     <>
       <h1>Proyectos</h1>
@@ -70,7 +100,16 @@ export const ProyectsList = () => {
               Fecha de Inicio
             </StyledTableCell>
             <StyledTableCell align="center" className={$.textColor}>
-              Solicitudes de compras
+              Convocatoria
+            </StyledTableCell>
+            <StyledTableCell align="center" className={$.textColor}>
+              Presupuesto total
+            </StyledTableCell>
+            <StyledTableCell align="center" className={$.textColor}>
+              Gastado
+            </StyledTableCell>
+            <StyledTableCell align="center" className={$.textColor}>
+              Remanente
             </StyledTableCell>
           </StyledTableHead>
           <TableBody>
@@ -91,7 +130,16 @@ export const ProyectsList = () => {
                   {formatDate(proyecto.fechaInicio)}
                 </StyledTableCell>                
                 <StyledTableCell align="center">
-                  {proyecto.Compras.length} 
+                  {proyecto.convocatoria.nombre} 
+                </StyledTableCell>
+                <StyledTableCell align="center">
+                  {'$'+proyecto.SubsidiosAsignados}
+                </StyledTableCell>
+                <StyledTableCell align="center">
+                  {'$'+proyecto.gastos}
+                </StyledTableCell>
+                <StyledTableCell align="center">
+                  {'$'+(proyecto.SubsidiosAsignados-proyecto.gastos)}
                 </StyledTableCell>
               </StyledTableRow>
             ))}

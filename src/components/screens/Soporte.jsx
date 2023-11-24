691fdab9ad515bba0e09625620584df3537635d0
JulianDiani <72026658+JulianDiani@users.noreply.github.com>
2023-11-24 20:01:55 -0300

    Mejoras en la pestaña de soporte

--- src/components/screens/Soporte.jsx
+++ src/components/screens/Soporte.jsx
@@ -1,17 +1,17 @@
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import {Container, Paper} from '@material-ui/core';
+import {Container, Paper,Card} from '@material-ui/core';
 
 const Soporte = ()=>{
   const $ = useStyles();
   return (
     <>
+    <div className={$.root}>
     <div className={$.titleAndButton}>
           <h1>Soporte</h1>
-        </div>
-    <Container maxWidth="xl" >      
-      <Paper style={{ padding: '1%', textAlign: 'center' }}>
-        <Typography variant="h2" gutterBottom>
+        </div>     
+    <Card className={$.paper}>
+        <Typography variant="h4" gutterBottom>
           ¡Soporte Técnico Personalizado a un <a href="mailto:soporte_unahur@gmail.com?subject=Consulta%20-%20Proyecto%20-%20Investigacion">
           Clic! 
                  </a>
@@ -26,32 +26,32 @@ const Soporte = ()=>{
           <strong>¿Por qué elegir nuestro soporte técnico por email?</strong>
         </Typography>
         <Typography paragraph>
-          <strong>1. Respuesta Rápida:</strong> Nuestro compromiso es proporcionar
+          <strong>1. Respuesta rápida:</strong> Nuestro compromiso es proporcionar
           respuestas rápidas y soluciones efectivas a tus problemas. Valoramos tu tiempo y 
           sabemos lo crítico que es resolver los problemas de manera eficiente.
         </Typography>
         <Typography paragraph>
-          <strong>2. Asistencia Personalizada:</strong> Cada cliente es único, y
+          <strong>2. Asistencia personalizada:</strong> Cada usuario es único, y
           nuestro equipo de soporte técnico se esfuerza por comprender tus
           necesidades específicas. Nos aseguramos de adaptar nuestras respuestas a
           tus circunstancias individuales para ofrecer soluciones que se ajusten
           perfectamente a tu situación.
         </Typography>
         <Typography paragraph>
-          <strong>3. Profesionales Altamente Calificados:</strong> Nuestro equipo
+          <strong>3. Profesionales altamente calificados:</strong> Nuestro equipo
           está compuesto por expertos altamente capacitados y dedicados a ofrecerte
           la mejor asistencia técnica. Nos mantenemos actualizados con las últimas
           tendencias y tecnologías para garantizar que recibas la información más
           precisa y actualizada.
         </Typography>
         <Typography paragraph>
-          <strong>4. Proceso Sencillo:</strong> Enviar tus consultas o problemas
+          <strong>4. Proceso sencillo:</strong> Enviar tus consultas o problemas
           técnicos es tan fácil como un clic. Simplemente responde a este correo
           electrónico, y nuestro equipo se pondrá en marcha para proporcionarte la
           ayuda que necesitas.
         </Typography>
         <Typography paragraph>
-          <strong>5. Seguimiento Continuo:</strong> No nos detenemos después de
+          <strong>5. Seguimiento continuo:</strong> No nos detenemos después de
           proporcionar una solución. Nuestro equipo realiza un seguimiento
           constante para asegurarse de que la asistencia que proporcionamos
           satisface completamente tus necesidades y expectativas.
@@ -62,14 +62,15 @@ const Soporte = ()=>{
           tecnología funcionando sin problemas!
         </Typography>
         <Typography variant="subtitle1">
-          Atentamente <strong>Soporte Tecnico</strong>
+          Atentamente <strong>Soporte técnico</strong>
           <br />
-          <strong>Contactanos aca: </strong> <a href="mailto:soporte_unahur@gmail.com?subject=Consulta%20-%20Proyecto%20-%20Investigacion">
+          <strong>Contacto: </strong> <a href="mailto:soporte_unahur@gmail.com?subject=Consulta%20-%20Proyecto%20-%20Investigacion">
                 soporte_unahur@gmail.com 
                  </a> (Consulta de Proyecto de Investigación)
         </Typography>
-      </Paper>
-    </Container>
+      </Card>
+    
+    </div>
     </>
         
      
